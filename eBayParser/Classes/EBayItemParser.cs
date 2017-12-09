02cb7ce46565a40117d47a442deb852e4a1fcbf9
David Frlic <david.frlic@gmail.com>
2017-12-09 12:13:23 +0100

    Added EBayItemParser class

---
+++ eBayParser/Classes/EBayItemParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eBayParser.Classes
+{
+    public static class EBayItemParser
+    {
+
+    }
+}
